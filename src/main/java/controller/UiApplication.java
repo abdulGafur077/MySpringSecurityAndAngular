80a02d8c5d24c0753e47c722702fc6dc028e2c42
abdulGafur077 <gafurbinnazer@gmail.com>
2018-11-25 15:33:02 +0530

    Second Commit : After adding the Ui and backend classes. And also added proxy.config.json remove CORS.

---
+++ src/main/java/controller/UiApplication.java
@@ -0,0 +1,34 @@
+package controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * Created by Abdul on 11/25/2018.
+ */
+
+@SpringBootApplication
+@RestController
+public class UiApplication {
+
+  @RequestMapping("/api/resource")
+  @CrossOrigin(origins = "/**")
+  public Map<String,Object> home() {
+    Map<String,Object> model = new HashMap<String,Object>();
+    model.put("id", UUID.randomUUID().toString());
+    model.put("content", "Hello World");
+    return model;
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(UiApplication.class, args);
+  }
+
+}
